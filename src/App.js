9915940236e98cbf6b66c1f73ca113cfc92c1ff7
shamim reza <reza_cse08@hotmail.com>
2020-10-12 23:51:33 +0600

    feature

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          New feature implemented
         </a>
       </header>
     </div>
