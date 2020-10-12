5885dcd818234e547ca1d3198dc87a33834c4d5e
shamim reza <reza_cse08@hotmail.com>
2020-10-12 23:59:49 +0600

    test

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          New feature implemented
+          New feature XX
         </a>
       </header>
     </div>
